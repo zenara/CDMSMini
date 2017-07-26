a71d4618c748734ff80131d2e66f09e9ce1c56a6
zenara <zenaronline@gmail.com>
2017-07-26 23:27:55 +0530

    Email Form

--- CDMS/Controllers/HomeController.cs
+++ CDMS/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CDMS.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace CDMS.Controllers
 {
@@ -27,11 +31,42 @@ namespace CDMS.Controllers
             return View();
         }
 
-        public ActionResult Statistics()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
         {
-            ViewBag.Message = "View the Statistics.";
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("zenaronline@gmail.com"));  // replace with valid value 
+                message.From = new MailAddress("chathurikasenani@outlook.com");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
 
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "chathurikasenani@outlook.com",  // replace with valid value
+                        Password = "outoutout1"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Sent()
+        {
             return View();
         }
+
     }
 }
